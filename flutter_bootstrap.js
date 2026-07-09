a705cb6cbf1a40daa7a4f40c36727102e2e12ae1
Alisafir12 <umqtech2026@gmail.com>
2026-07-09 15:43:48 +0300

    firsts1

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"ddf47dd3ff96dbde6d9c614db0d7f019d7c7a2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1936997413"
+    serviceWorkerVersion: "1611173123"
   }
 });
